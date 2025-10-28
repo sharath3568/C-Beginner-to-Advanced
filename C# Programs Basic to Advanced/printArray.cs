2ad92402f0b3151b2d6ff3fbf19c7ac39740c8dd
Sharath Jettoji (INFOSYS LIMITED) <v-sjettoji@microsoft.com>
2025-10-28 22:51:11 +0530

    Basic Printing the values of array taken from the user

---
+++ C# Programs Basic to Advanced/printArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Programs_Basic_to_Advanced
+{
+    internal class printArray
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the count of the numbers you want to enter : ");
+            int count = checkValid();
+            int[] number = new int[count];
+
+            Console.WriteLine("Enter the Numbers : ");
+            for(int i = 0; i < count; i++)
+            {
+                number[i] = checkValid();
+            }
+
+            Console.WriteLine("Numbers you have entered are : ");
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write($"{number[i]} ");
+            }
+        }
+
+        public static int checkValid()
+        {
+            while (true) {
+                bool check = int.TryParse(Console.ReadLine(), out int value);
+                if (check && value > 0) {
+                    return value;
+                }
+                else
+                {
+                    Console.Write("Invalid input! Please try again : ");
+                }
+            }
+        }
+    }
+}
